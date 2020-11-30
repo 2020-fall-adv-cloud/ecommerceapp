aa05f13ed77bb01e2ec96f95f57fdbb42ca17544
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-11-30 12:48:06 -0600

    Improving the code...
    True try/catch refactoring from the book mixed async/await & promise code : - (

--- src/checkUser.js
+++ src/checkUser.js
@@ -5,17 +5,6 @@ const checkUser = async (updateUser) => {
     try {
         const userData = await Auth.currentSession()
 
-        // Sentinel, guard code, stop executing the checkUser function if this isn't working... 
-        if (!userData) {
-            console.log('userData: ', userData);
-
-            // Call the callback with an empty object.
-            updateUser({});
-
-            // Stop running this function.
-            return;
-        }
-
         console.log(userData);
 
         // Complex destructuring... E.g. Nested property destructuring... Could use userData.idToken.payload
